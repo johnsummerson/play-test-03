0aab4f1a067cd706df6987f8048e1e6a035c0158
John Summerson <johnsummerson@Johns-MacBook-Pro-2.local>
2024-07-21 17:26:39 -0400

    testing playcanvas to github pages

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2038402.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 189247878, 189248836, 189248838, 189248839, 189248840, 189248841, 189248845 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/189247881/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/189247882/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/189247880/1/ammo.js', 'preload' : true},
+];
